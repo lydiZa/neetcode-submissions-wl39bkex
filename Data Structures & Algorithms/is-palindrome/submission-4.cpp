168aeed99bd0c431500e53423507cdb9f2eaecd8
lydiZa <162075871+lydiZa@users.noreply.github.com>
2026-04-16 20:13:10 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s2;
+        for(int i=0;i<s.size();i++){
+            if(isalnum(s[i])){
+                s2+=tolower(s[i]);
+            }
+        }
+        int left = 0;
+        int right = s2.size()-1;
+        while(left<right){
+          
+            if(s2[left]!=s2[right]){
+                return false;
+            }else{
+                left++;
+                right--;
+            }
+        
+        }
+        return true;
+    }
+};
